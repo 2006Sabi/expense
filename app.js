94bf114b5f6e3424f71d9b1575c5bcaed6298833
sabishree <sabishree06@gmail.com>
2025-01-22 15:52:35 +0530

    commited

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 app.use(express.json());
 const { v4: uuidv4 } = require("uuid");
 
-mongoose.connect("mongodb://localhost:27017/expenses").then(() => {
-  console.log("Connect to MongoDB");
-});
+mongoose
+  .connect(
+    "mongodb+srv://sabishreer:Sabi5214@cluster0.vxoba.mongodb.net/expenses"
+  )
+  .then(() => {
+    console.log("Connect to MongoDB");
+  });
 const expenseSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   title: { type: String, required: true },
